b003fd8f1164fa6979c7439d71664550b00713eb
100161792 <leodefarias25@gmail.com>
2022-03-01 08:49:44 -0600

    Finished

--- Bronze.java
+++ Bronze.java
@@ -1,7 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+
 public class Bronze {
 
-    private final int N = 10;
-    private final int K = 7;
+    private static int N = 10;
+    private static int K = 7;
+
+    public Bronze(String N&K) {
+        this.N = Integer.parseInt(N&K.split(" ")[0]);
+        this.K = Integer.parseInt(N&K.split(" ")[1]);
+    }
 
     public void wordProcessor(String line) {
 
@@ -23,8 +33,13 @@ public class Bronze {
     }
 
 
-    public static void main(String[] args) {
-        Bronze bronze = new Bronze();
+    public static void main(String[] args) throws new IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(r.readLine());
+        String constraints = st.nextToken();
+        Bronze bronze = new Bronze(constraints);
         bronze.wordProcessor("hello my name is Bessie and this is my essay");
+        pw.close();
     }
 }
